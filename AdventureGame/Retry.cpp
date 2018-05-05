46efe771c982cec0594b4fa5c0ea954fcec0f706
Joseph Garrett <joegarrett132@hotmail.com>
2018-05-05 22:51:33 +0100

    Added QuestOneIntro

--- AdventureGame/Retry.cpp
+++ AdventureGame/Retry.cpp
@@ -4,20 +4,34 @@
 
 using namespace std;
 
+class Wallet
+{
+	public:
+		int Gold;
+		int Silver;
+		int Bronze;	
+		int CrestedEagle;	
+		Wallet(void) : Gold(10), Silver(50), Bronze(250) {}	
+};
+
 class PartyMember
 {
 	public:
-		PartyMember(int S, int A, int I, string N) : Name(N), Strength(S), Agility(A), Intelect(I) {}
+		PartyMember(int S, int A, int I, string N) : Name(N), Strength(S), Agility(A), Intelect(I), Health(S * 2), Stamina(A * 2), Magika(I * 2) {}
 		static PartyMember* make_member(int Choice, string N);
 		virtual void Shout(void) = 0;
 		bool StrengthTest(int Pass){return Pass <= Strength;}
 		bool AgilityTest(int Pass){return Pass <= Agility;}
 		bool IntelectTest(int Pass){return Pass <= Intelect;}
+		Wallet wallet;
 	private:
 		string Name;
 		int Strength;
 		int Agility;
 		int Intelect;
+		int Health;
+		int Stamina;
+		int Magika;
 };
 
 class Warrior : public PartyMember
@@ -54,7 +68,7 @@ PartyMember* PartyMember::make_member(int Choice, string N)
 	return NULL;
 }
 
-PartyMember* Setup(void)
+PartyMember* InitialEncounter(void)
 {
 	
 	vector<string> AllClasses = {"1. Warrior", "2. Mage"};
@@ -84,10 +98,51 @@ PartyMember* Setup(void)
 	while(true);
 }
 
+void QuestOneIntro(vector<PartyMember*> Party)
+{
+	cout << "Nice to meet you, I'm Gregor Karlsson, innkeeper here at the Bearded Woman." << endl;
+	cout << "Say, an adventurer like yourself wouldn't be looking for a job would you, put a little cash in your pocket?" << endl;
+	cout << "1. Politely decline, you don't need the money anyway" << endl;
+	cout << "2. Ask for more details, you can't agree without them" << endl;
+	cout << "3. You had me at money!" << endl;
+	int Choice_1;
+	cin >> Choice_1;
+	int Choice_2;
+	switch (Choice_1)
+	{
+		case 1:
+			Choice_2 = QuestOneIntro_One(void);
+		case 2:
+			cout << "A smart one you are, well, I'll tell you more then" << endl;
+			cout << "About a week ago, Betty, my eldest daugther, went down to the basement and she saw something big" << endl;
+			cout << "Being the man of this place, I went down to chase it out" << endl;
+			cout << "As it turned out, it was a giant spider, about two feet off the ground" << endl;
+			cout << "Anyway, I gave it a good wack, and it ran off into the forest, just east of here" << endl;
+			cout << "I think it's got a nest down by the river, and the spiders have been giving me trouble ever since it happened" << endl;
+			cout << "I can't leave here, but I recon a person of your strengths should be able to handle it, how does it sound?" << endl;	
+			cout << "1. And the reward?" << endl;
+			cout << "2. Of course, anything for a fellow citizen of the realm" << endl;
+		case 3:
+			cout << "Ok, well there is a cave full of spiders in the woods to the east, get rid of them and I'll pay you a king's randsom" << endl;
+		case default:
+	}
+}
+int QuestOneIntro_One(void)
+{
+	cout << "Aww well. A room is 2 silver pieces a night, or 10 for the nicer rooms at the top" << endl;
+	cout << "1. Hand over 2 silver for a room" << endl;
+	cout << "2. Pay the 10, it's been a long day on the road, you want the nicer bed" << endl;
+	cout << "3. You don't need the room, you're just here for a drink" << endl;  
+	int Choice;
+	cin >> Choice;
+	return Choice;
+}
+		
+
 int main(void)
 {
 	vector<PartyMember*> Party;
-	Party.push_back(Setup());
+	Party.push_back(InitialEncounter());
 	Party[0]->Shout();
 	cout << Party[0]->StrengthTest(8) << endl;
 	return 0;
